4c18761abd59c325dd5aa5aa55c5ab8c85e36d1b
Varsha Natarajan <varsha.natarajan@colorado.edu>
2023-10-23 02:55:59 -0600

    cloudlab v1: rdma setup

--- profile.py
+++ profile.py
@@ -49,7 +49,7 @@ for i in range(params.nodeCount):
 
 for i, node in enumerate(nodes):
     # Install and execute a script that is contained in the repository.
-    node.addService(pg.Execute(shell="sh", command="/local/repository/start.sh {} {} > /home/rdma-{}/start.log 2>&1".format(params.rdmaType,
+    node.addService(pg.Execute(shell="sh", command="/local/repository/start.sh {} {} > /local/repository/rdma-{}-start.log 2>&1".format(params.rdmaType,
                                                                                                                             "{}.{}".format(BASE_IP, 1 + i), params.rdmaType)))
 
 # Print the RSpec to the enclosing page.
--- start.sh
+++ start.sh
@@ -3,44 +3,44 @@
 echo "This is a silly script" > /tmp/silly.txt
 
 initial_setup() {
-  printf "Updating packages..."
+  printf "Updating packages...\n"
   sudo apt-get update
-  printf "Install IBVerbs, RDMA_CM and utils"
+  printf "Install IBVerbs, RDMA_CM and utils \n"
   sudo apt-get install build-essential libelf-dev cmake
   sudo apt-get install libibverbs1 libibverbs-dev librdmacm1 librdmacm-dev rdmacm-utils ibverbs-utils
 }
 
 add_ib_core() {
-  printf "ModProbe ib_core"
+  printf "ModProbe ib_core \n"
   sudo modprobe ib_core
 }
 
 add_rdma_cm() {
-  printf "ModProbe rdma_cm"
+  printf "ModProbe rdma_cm \n"
   sudo modprobe rdma_cm
 }
 
 add_rdma_rxe() {
-  printf "Installing rdma_rxe"
+  printf "Installing rdma_rxe \n"
   sudo modprobe rdma_rxe
-  printf "Verifying..."
+  printf "Verifying... \n"
   sudo lsmod | grep rxe
 }
 
 add_siw() {
-  printf "Installing siw"
+  printf "Installing siw \n"
   sudo modprobe siw
-  printf "Verifying..."
+  printf "Verifying... \n"
   sudo lsmod | grep siw
 }
 
 link_rdma_device() {
-  printf "Link RDMA"
+  printf "Link RDMA \n"
   sudo rdma link add t_siw type "$1" netdev "$ifname"
 }
 
 install_essential_pkgs() {
-  printf "Installing other packages..."
+  printf "Installing other packages... \n"
   sudo apt-get install build-essential cmake gcc libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind
 }
 
@@ -52,7 +52,7 @@ if [ "$1" == "siw" ]; then
 fi
 
 ifname=$(ip route list "$2""/24" | awk '{print $3}')
-printf "%s" "$ifname"
+printf "%s\n" "$ifname"
 
 link_rdma_device "$1" "$ifname"
 
