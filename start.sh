1e912001cf9cea4d6b0c089c62e0af35c71f33d8
Adarsh Srinivasan <adarsh.srinivasan@colorado.edu>
2023-12-06 00:28:45 -0700

    install docker-compose in node
    Signed-off-by: Adarsh Srinivasan <adarsh.srinivasan@colorado.edu>

--- start.sh
+++ start.sh
@@ -53,10 +53,13 @@ link_rdma_device() {
 
 install_docker() {
   printf "Installing docker \n"
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sudo sh get-docker.sh
+  curl -fsSL https://get.docker.com -o ~/get-docker.sh
+  sudo sh ~/get-docker.sh
   sudo usermod -aG docker $USER
   newgrp docker
+  mkdir -p ~/.docker/cli-plugins/
+  curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  chmod +x ~/.docker/cli-plugins/docker-compose
 }
 
 initial_setup
